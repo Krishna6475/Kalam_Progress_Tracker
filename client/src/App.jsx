6dbd08e1803de4912c7ec2573f36b116a0918928
Chinthagunta Vamshi Krishna <vamshikrishna6475@gmail.com>
2025-06-30 20:07:41 +0530

    added all files and folders related to the project

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline text-center mt-10">
+        Welcome to Team 7's Project!
+      </h1>
+      <p className="text-center mt-5">
+        This is a simple React application styled with Tailwind CSS.
+      </p>
+    </div>
+  );
+};
+
+export default App;
